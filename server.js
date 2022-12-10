e13d56ed01686c844655b8d9d1b85414b3e9f45f
NickBwalley <nickbwalley@gmail.com>
2022-12-11 01:23:54 +0300

    broadcast emitters in sockets

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
-// set static folder
+// Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
+
+// Run when a client connects
+io.on('connection', (socket) => {
+  // Welcome current user
+  socket.emit('message', 'Welcome to ChartCord!.')
+
+  // Broadcast when a user connects
+  socket.broadcast.emit('message', 'A user has joined the chat')
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left the chat!')
+  })
+})
 
 const PORT = 3000 || process.env.PORT
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
