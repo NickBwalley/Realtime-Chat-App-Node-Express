e186545189031d57e638a8da81ef9b52c7852559
NickBwalley <nickbwalley@gmail.com>
2022-12-17 09:48:28 +0300

    userjoined and userleft functionality done

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
 const formatMessage = require('./utils/messages')
-const { userJoin, getCurrentUser } = require('./utils/users')
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require('./utils/users')
 
 const app = express()
 const server = http.createServer(app)
@@ -42,7 +47,14 @@ io.on('connection', (socket) => {
 
   // Runs when client disconnects
   socket.on('disconnect', () => {
-    io.emit('message', formatMessage(botName, 'A user has left the chat.'))
+    const user = userLeave(socket.id)
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(botName, `${user.username} has left the chat.`)
+      )
+    }
   })
 })
 
--- utils/users.js
+++ utils/users.js
@@ -14,7 +14,23 @@ const getCurrentUser = (id) => {
   return users.find((user) => user.id === id)
 }
 
+// User leaves chat
+const userLeave = (id) => {
+  const index = users.findIndex((user) => user.id === id)
+
+  if (index != -1) {
+    return users.splice(index, 1)[0]
+  }
+}
+
+// Get room users
+const getRoomUsers = (room) => {
+  return users.filter((user) => user.room === room)
+}
+
 module.exports = {
   userJoin,
   getCurrentUser,
+  userLeave,
+  getRoomUsers,
 }
